a84dd2ccae94e0fbab5d791cc775c0e823fc1c25
Ahaan Kotian <ahaankotian7@Ahaans-MacBook-Air.local>
2024-11-06 01:18:15 +0530

    add tl_hybrid.py

--- hybrid/hybrid_model.py
+++ hybrid/hybrid_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe2d25ad277eb3a22e1508c0288ce571f0b2b1e994ca6c60b7ac413b800f8492
-size 11777
+oid sha256:a0e4589dba79e0b7d7dc04e36f2a688e194b9f9bdc2a746fbd9b819da4fbede1
+size 11783
